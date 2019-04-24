db20e751d3ce5c35ce7b9caf12865829b3f0042b
Abubakar Omolaja G <abubakar.omolaja@outlook.com>
2019-04-24 12:55:06 +0100

    Add shuttercart to repo

---
+++ ShutterCart.Test/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ShutterCart.Test.Controllers
+{
+   public class CustomerController
+    {
+        [Fact]
+        public bool TestCustomerNameLength()
+        {
+            string customername = "Omolaja";
+
+            int CustomernameLenth = 4;
+
+          //  Asset.Equal(CustomernameLenth, customerName.Length);
+
+            return (CustomernameLenth == customername.Length);
+
+        }
+
+
+    }
+}
